f298838519faf56abefbf3ecda043b3ae50cfee1
AditSarda <81905978+AditSarda@users.noreply.github.com>
2021-10-20 21:38:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var canvas = document.querySelector("#myCanva");
 var context = canvas.getContext("2d");
 
-context.beginPath();
-context.rect(0, 300, 400, 300);
+context.beginPath()
+context.font = "16px Aerial";
+context.fillText("A sailor's joy are as simple as a child's.", 5, 20);
+context.fill();
 
 var lGradient = context.createLinearGradient(0, 0, 400, 0);
 lGradient.addColorStop(0, "#0000FF");
 lGradient.addColorStop(1, "#5F9AD1");
 
+context.beginPath();
+context.bezierCurveTo(0, 270, 350, 350, 400, 290);
+context.lineTo(400, 400);
+context.lineTo(0, 400);
+context.closePath();
 context.fillStyle = lGradient;
 context.strokeStyle = "blue";
 context.fill();
@@ -15,14 +22,29 @@ context.stroke();
 
 
 context.beginPath();
-context.moveTo(155, 150);
-context.lineTo(220, 270);
-context.lineTo(90, 270);
-context.lineTo(155, 150);
+context.moveTo(145, 145);
+context.quadraticCurveTo(95, 180, 85, 260);
+context.quadraticCurveTo(105, 250, 145, 260);
+context.quadraticCurveTo(135, 200, 145, 145);
 context.fillStyle = "white";
 context.strokeStyle = "white";
 context.fill();
 context.stroke();
+
+context.beginPath();
+context.moveTo(165, 145);
+context.lineTo(165, 260);
+context.lineTo(225, 260);
+context.fillStyle = "white";
+context.strokeStyle = "white";
+context.fill();
+context.stroke();
+
+context.beginPath();
+context.font = "700 20px sans-serif";
+context.strokeStyle = "blue";
+context.strokeText("USA", 167, 250);
+context.fill();
 
 context.beginPath();
 context.moveTo(145, 110);
@@ -48,14 +70,47 @@ context.strokeStyle = "black";
 context.fill();
 context.stroke();
 
+var mouseX = 330;
+var mouseY = 70;
+
+function drawSun() {
+
+    context.beginPath();
+    context.arc(mouseX, mouseY, 45, 0, 2 * Math.PI, true);
+
+    var rGradient = context.createRadialGradient(mouseX, mouseY, 10, mouseX, mouseY, 45);
+    rGradient.addColorStop(0, "#FF8C00");
+    rGradient.addColorStop(1, "#FFFF00");
+
+    context.fillStyle = rGradient;
+    context.strokeStyle = "yellow";
+    context.fill();
+    context.stroke();
+}
+
+function getPosition(event) {
+    context.clearRect(mouseX - 50, mouseY - 50, 100, 100)
+    mouseX = event.clientX - canvas.offsetLeft;
+    mouseY = event.clientY - canvas.offsetTop;
+    drawSun();
+}
+
+drawSun();
+
+canvas.addEventListener("mousemove", getPosition, false);
+
+//Birds
 context.beginPath();
-context.arc(330, 70, 45, 0, 2 * Math.PI, true);
+context.moveTo(50, 50);
+context.quadraticCurveTo(70, 70, 80, 50);
+context.quadraticCurveTo(90, 70, 110, 50);
+context.strokeStyle = "black";
+context.stroke();
 
-var rGradient = context.createRadialGradient(330, 70, 10, 330, 70, 45);
-rGradient.addColorStop(0, "#FF8C00");
-rGradient.addColorStop(1, "#FFFF00");
-
-context.fillStyle = rGradient;
-context.strokeStyle = "yellow";
-context.fill();
+//Bird2
+context.beginPath();
+context.moveTo(80, 80);
+context.quadraticCurveTo(100, 100, 110, 80);
+context.quadraticCurveTo(120, 100, 140, 70);
+context.strokeStyle = "black";
 context.stroke();
